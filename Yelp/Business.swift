85ee0aa8ed7e8fad713afe45cc0ece6f49757970
parry <parry@parrys-air.attlocal.net>
2016-10-24 02:17:53 -0700

    trying to work around enum and fetch data

--- Yelp/Business.swift
+++ Yelp/Business.swift
@@ -88,6 +88,11 @@ import UIKit
         _ = YelpClient.sharedInstance.searchWithTerm(term, completion: completion)
     }
     
+    class func searchWithCategories(term: String, categories: [String]?, completion: @escaping ([Business]?, Error?) -> Void) -> Void {
+        _ = YelpClient.sharedInstance.searchWithTerm(term, sort: nil, categories: categories, deals: nil, completion: completion)
+    }
+    
+    
     class func searchWithTerm(term: String, sort: YelpSortMode?, categories: [String]?, deals: Bool?, completion: @escaping ([Business]?, Error?) -> Void) -> Void {
         _ = YelpClient.sharedInstance.searchWithTerm(term, sort: sort, categories: categories, deals: deals, completion: completion)
     }
