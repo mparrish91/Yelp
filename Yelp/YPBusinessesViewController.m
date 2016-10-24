91d35ffda281bd9cbdd13530cd0109ccac5dab30
parry <parry@parrys-air.attlocal.net>
2016-10-24 14:23:11 -0700

    search up

--- Yelp/YPBusinessesViewController.m
+++ Yelp/YPBusinessesViewController.m
@@ -425,6 +425,44 @@
     NSLog(@"User searched for %@", searchBar.text);
    NSString *queryString = searchBar.text;
     [searchBar resignFirstResponder];
+    
+    [Business searchWithTermWithTerm:queryString completion:^(NSArray *objects, NSError *error)
+     {
+         
+         if (error)
+         {
+             [self showErrorView:self.errorView];
+         }
+         else
+         {
+             [self hideErrorView:self.errorView];
+             
+         }
+         self.businesses = objects;
+         self.displayedItems = self.businesses;
+         
+         
+         dispatch_async(dispatch_get_main_queue(), ^{
+             self.isMoreDataLoading = false;
+             [self.businessesTableView reloadData];
+             
+             
+             
+             if ([[NSThread currentThread] isMainThread]){
+                 NSLog(@"In main thread--completion handler");
+                 [self.refreshControl endRefreshing];
+                 [self.loadingMoreView stopAnimating];
+                 
+                 
+             }
+             else{
+                 NSLog(@"Not in main thread--completion handler");
+             }
+             
+         });
+         
+     }];
+
 
 }
 
